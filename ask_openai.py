f75bb9f16c5b3cd35c8a2ddf6f7b4bd96cd24df0
Luka Milic <lukam321@gmail.com>
2022-11-04 23:24:54 +0000

    first commit

--- ask_openai.py
+++ ask_openai.py
@@ -2,18 +2,18 @@ import os
 import openai
 openai.api_key = os.environ.get("OPENAI_KEY")
 
-MODEL="davinci-instruct-beta"
+MODEL="text-davinci-002"
 
-def ask_prompt(prompt, model=MODEL, num_results=1, max_tokens=25, stopSequences=["You:", "Kirby:"],
-                  temperature=0.8, topP=1.0, topKReturn=2):
+def ask_prompt(prompt, model=MODEL, num_results=1, max_tokens=250, stopSequences=["You:", "Zizek:"],
+                  temperature=1.0, topP=1.0, topKReturn=2):
     response = openai.Completion.create(
         engine=model,
         prompt=prompt,
         temperature=temperature,
         max_tokens=max_tokens,
         top_p=topP,
-        frequency_penalty=0.3,
-        presence_penalty=0.3,
+        frequency_penalty=2.0,
+        presence_penalty=2.0,
         stop=stopSequences
     )
     if response != 0:
